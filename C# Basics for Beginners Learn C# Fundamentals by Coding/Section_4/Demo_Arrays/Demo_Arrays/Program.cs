1eadcbabd93707641b1a5e9ea73cf7e5d430b990
Amir Harrison <haveacompleteday@gmail.com>
2021-07-01 17:48:59 -0600

    Arrays Demo

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_4/Demo_Arrays/Demo_Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Demo_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var family = new string[4] {"Amir", "Stormy", "Charlee", "Rigby"};
+
+            Console.WriteLine(family[0]);
+            Console.WriteLine(family[1]);
+            Console.WriteLine(family[2]);
+            Console.WriteLine(family[3]);
+        }
+    }
+}
