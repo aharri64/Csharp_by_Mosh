9c8dfa4e1a8d3a559a333009ae228b4871f56a1d
Amir Harrison <haveacompleteday@gmail.com>
2021-07-01 18:30:45 -0600

    demo strings

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_4/Demo-Strings/Demo-Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Amir";
+            var lastName = "Harrison";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My Name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Jeremy"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+
+            var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+            //or
+            var text2 = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text2);
+        }
+    }
+}
