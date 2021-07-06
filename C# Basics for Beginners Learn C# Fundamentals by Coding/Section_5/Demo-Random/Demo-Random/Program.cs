c61f34e0227179474940c4c3e5d33b6765f3d436
Amir Harrison <haveacompleteday@gmail.com>
2021-07-05 21:55:18 -0600

    learned about random

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_5/Demo-Random/Demo-Random/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine(random.Next());
+            
+            PARAMETERS
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine(random.Next(1, 10));
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+                Console.Write((char)random.Next(97, 122));
+            Console.WriteLine((int)'a');
+            */
+
+            var random = new Random();
+
+            const int passwordLength = 3;
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+
+
+
+        }
+    }
+}
