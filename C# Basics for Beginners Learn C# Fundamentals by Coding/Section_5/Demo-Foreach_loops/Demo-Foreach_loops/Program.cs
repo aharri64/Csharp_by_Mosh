f1bdbf07f6d664abf641782bf7d0444b4f9026a8
Amir Harrison <haveacompleteday@gmail.com>
2021-07-05 21:07:33 -0600

    foreach loop demo

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_5/Demo-Foreach_loops/Demo-Foreach_loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Foreach_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loop
+            /*
+            var name = "John Smith";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            //FOREACH LOOP
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+            */
+
+            var numbers = new int[] {1, 2, 3, 4};
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
