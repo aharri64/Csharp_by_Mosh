40fb0a7c65ed541c10ce9b548f09c38a81e2f6ab
Amir Harrison <haveacompleteday@gmail.com>
2021-07-05 21:38:47 -0600

    while loops, continue, and break

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_5/Demo-While_loops/Demo-While_loops/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_While_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for (var i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            SAME THING AS A WHILE LOOP
+            var i = 0;
+
+            while (i <= 10)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+
+                i++;
+            }
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+
+                Console.WriteLine("@Echo: " + input);
+            }
+            */
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+
+
+        }
+    }
+}
