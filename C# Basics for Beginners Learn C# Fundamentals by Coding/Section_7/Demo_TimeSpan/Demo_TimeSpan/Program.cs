c7d8382bfbe1d8d09fe9778f489ecc4299809a45
Amir Harrison <haveacompleteday@gmail.com>
2021-07-15 14:58:22 -0600

    timespan demo

--- C# Basics for Beginners Learn C# Fundamentals by Coding/Section_7/Demo_TimeSpan/Demo_TimeSpan/Program.cs
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_7/Demo_TimeSpan/Demo_TimeSpan/Program.cs
@@ -10,6 +10,32 @@ namespace Demo_TimeSpan
     {
         static void Main(string[] args)
         {
+            // Creating TimeSpan Objects
+            var timeSpan = new TimeSpan(1, 5, 3);
+            
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timespan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            
+            // Subtract
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(8)));
+
+            // ToString
+            Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
