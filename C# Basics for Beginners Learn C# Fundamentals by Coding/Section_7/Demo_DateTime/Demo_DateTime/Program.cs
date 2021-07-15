c209fc91d3e07a27bfbb9b95b64b37eaf5131643
Amir Harrison <haveacompleteday@gmail.com>
2021-07-15 14:31:04 -0600

    demo_DateTime

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_7/Demo_DateTime/Demo_DateTime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2021, 1, 1);
+            
+            // To get current dateTime
+            var now = DateTime.Now;
+
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            // datetime is immutable
+
+            // You can change them with different methods that all start with add
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            // formatting DateTime to a string
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("MMMM dd, yyyy hh:mm"));
+        }
+    }
+}
