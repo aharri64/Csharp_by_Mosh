d6b1bf6d304e81040551d6317e8da3b286890255
Amir Harrison <haveacompleteday@gmail.com>
2021-07-14 19:14:55 -0600

    demo_lists

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_6/Demo_Lists/Demo_Lists/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+
+            Console.WriteLine();
+
+            // IndexOf()
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Last Index of 1: " + numbers.LastIndexOf(1));
+            
+            // Count
+            Console.WriteLine("Count: " + numbers.Count);
+
+            numbers.Remove(1);
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine();
+            // If you wanted to remove all the 1's lets add a few ones to remove
+            numbers.Add(1);
+            numbers.Add(1);
+            numbers.Add(1);
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine();
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+ 
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine();
+
+            // clear method
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
+
+
+        }
+    }
+}
