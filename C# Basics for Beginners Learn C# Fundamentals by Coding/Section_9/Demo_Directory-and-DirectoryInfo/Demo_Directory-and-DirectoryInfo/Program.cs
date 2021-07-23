c4ef1e0480ed5b211a84334c5671887f9defb2f3
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 15:12:18 -0600

    Demo: Directory and DirectoryInfo

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_9/Demo_Directory-and-DirectoryInfo/Demo_Directory-and-DirectoryInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Directory_and_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            Directory.CreateDirectory(path);
+
+            var files = Directory.GetFiles(@"d:\Projects\Udemy\Csharp_by_Mosh", "*.md", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //    Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"d:\Projects\Udemy\Csharp_by_Mosh", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...Path");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
