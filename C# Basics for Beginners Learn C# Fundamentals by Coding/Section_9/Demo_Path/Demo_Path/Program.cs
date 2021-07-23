a7d9720ed28365ba1a40c64b21bda78854d1e00f
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 15:30:06 -0600

    Demo: Path

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_9/Demo_Path/Demo_Path/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Projects\Udemy\Csharp_by_Mosh";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("extension: " + Path.GetExtension(path));
+        }
+    }
+}
