70fc2311be95ce1d026de515affaeefda5320f9f
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-07-23 14:52:49 -0600

    Demo: File and FileInfo

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_9/Demo_File_FileInfo/Demo_File_FileInfo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_File_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg"
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg)", true;)
+
+            File.Delete(path);
+            if (File.Exists(path));
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+
+        }
+    }
+}
