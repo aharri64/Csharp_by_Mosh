4dfec7bdb34d9bc74ddf1c6813147d222f8fe5fa
Amir Harrison <76721355+aharri64@users.noreply.github.com>
2021-08-30 10:48:11 -0600

    org

---
+++ C# Basics for Beginners Learn C# Fundamentals by Coding/Section_5/ControlFlowProblems/ControlFlowProblems/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ControlFlowProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1- 
+            Write a program to count how many numbers 
+            between 1 and 100 are divisible by 3 with no remainder. 
+            Display the count on the console.
+            */
+
+            /* Answer
+            var count = 0;
+            for (var i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            Console.WriteLine("There are {0} numbers in between 0 and 100 that are divisible by 3", count);
+            */
+
+            /*
+            2- 
+            Write a program and continuously ask the user to enter a number or "ok" to exit. 
+            Calculate the sum of all the previously entered numbers and display it on the console.
+            */
+
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                {
+                    break;
+                }
+                else
+                {
+                    sum += Convert.ToInt32(input);
+                }
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+        }
+    }
+}
